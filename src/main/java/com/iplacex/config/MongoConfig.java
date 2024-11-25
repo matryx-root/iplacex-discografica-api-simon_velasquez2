3913816875e34bb2c992c3c6e51d67c035f283e1
Simon Velasquez c <root.matryx@gmail.com>
2024-11-25 13:39:05 -0300

    ultimo

--- src/main/java/com/iplacex/config/MongoConfig.java
+++ src/main/java/com/iplacex/config/MongoConfig.java
@@ -5,8 +5,9 @@ import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
+
     @Override
     protected String getDatabaseName() {
-        return "cine-db";
+        return "cine-db"; // Cambia "cine-db" por el nombre de tu base de datos
     }
 }
